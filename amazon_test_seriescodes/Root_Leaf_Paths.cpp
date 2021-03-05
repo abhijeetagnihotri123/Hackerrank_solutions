513dfd6da2569a0c15cb7303c3d115a131bff01e
abhijeetagnihotri123 <abhijeetagnihotri123>
2021-03-05 21:52:35 +0530

    more code added

--- amazon_test_seriescodes/Root_Leaf_Paths.cpp
+++ amazon_test_seriescodes/Root_Leaf_Paths.cpp
@@ -1,11 +1,13 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+#define MAX_HEIGHT 100000
 
+// Tree Node
 struct Node
 {
     int data;
-    struct Node *left;
-    struct Node *right;
+    Node* left;
+    Node* right;
 };
 Node* newNode(int val)
 {
@@ -13,89 +15,93 @@ Node* newNode(int val)
     temp->data = val;
     temp->left = NULL;
     temp->right = NULL;
-    
     return temp;
 }
+vector<vector<int>> Paths(Node *root);
 Node* buildTree(string str)
-{   
+{
     // Corner Case
     if(str.length() == 0 || str[0] == 'N')
-            return NULL;
-    
-    // Creating vector of strings from input 
+        return NULL;
+
+    // Creating vector of strings from input
     // string after spliting by space
     vector<string> ip;
-    
+
     istringstream iss(str);
     for(string str; iss >> str; )
         ip.push_back(str);
-        
+
     // Create the root of the tree
     Node* root = newNode(stoi(ip[0]));
-        
+
     // Push the root to the queue
     queue<Node*> queue;
     queue.push(root);
-        
+
     // Starting from the second element
     int i = 1;
     while(!queue.empty() && i < ip.size()) {
-            
+
         // Get and remove the front of the queue
         Node* currNode = queue.front();
         queue.pop();
-            
+
         // Get the current node's value from the string
         string currVal = ip[i];
-            
+
         // If the left child is not null
         if(currVal != "N") {
-                
+
             // Create the left child for the current node
             currNode->left = newNode(stoi(currVal));
-                
+
             // Push it to the queue
             queue.push(currNode->left);
         }
-            
+
         // For the right child
         i++;
         if(i >= ip.size())
             break;
         currVal = ip[i];
-            
+
         // If the right child is not null
         if(currVal != "N") {
-                
+
             // Create the right child for the current node
             currNode->right = newNode(stoi(currVal));
-                
+
             // Push it to the queue
             queue.push(currNode->right);
         }
         i++;
     }
-    
     return root;
 }
-long long treePathsSum(Node *root);
-
-int main()
-{
+int main() {
     int t;
-	scanf("%d ",&t);
+    string tc;
+    getline(cin, tc);
+    t=stoi(tc);
     while(t--)
     {
-        string s;
-		getline(cin,s);
-		Node* root = buildTree(s);
-		
-        cout<<treePathsSum(root);
-   		cout<<endl;
-  }
-  return 0;
+        string s ,ch;
+        getline(cin, s);
+        Node* root = buildTree(s);
+
+        vector<vector<int>> paths = Paths(root);
+        for(int i = 0;i<paths.size();i++){
+            for(int j = 0;j<paths[i].size();j++){
+                cout<<paths[i][j]<<" ";
+            }
+            cout<<"#";
+        } 
+        cout<<"\n";
+    }
+    return 0;
 }
-void preorder_sum(Node *root,long long &res,long long cur)
+void preorder(Node *root,vector<int>v,vector<vector<int>>&res)
 {
     if(root == NULL)
     {
@@ -103,30 +109,28 @@ void preorder_sum(Node *root,long long &res,long long cur)
     }
     else if(root->left == NULL && root->right == NULL)
     {
-        cur = cur*10+root->data;
-        res = res+cur;
+        v.push_back(root->data);
+        res.push_back(v);
     }
     else
     {
-        preorder_sum(root->left,res,cur*10+root->data);
-        preorder_sum(root->right,res,cur*10+root->data);
+        v.push_back(root->data);
+        preorder(root->left,v,res);
+        preorder(root->right,v,res);
     }
 }
-long long treePathsSum(Node *root)
+vector<vector<int>> Paths(Node* root)
 {
     if(root == NULL)
     {
-        return 0;
-    }
-    else if(root->left == NULL && root->right == NULL)
-    {
-        return root->data;
+        vector<vector<int>>res(0);
+        return res;
     }
     else
     {
-        long long res = 0;
-        long long curr = 0;
-        preorder_sum(root,res,curr);
+        vector<vector<int>>res;
+        vector<int>v;
+        preorder(root,v,res);
         return res;
     }
 }
