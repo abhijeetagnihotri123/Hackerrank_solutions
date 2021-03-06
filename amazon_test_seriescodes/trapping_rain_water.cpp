c4e28757b0f80fb1d28289010cd6a984535c57f6
abhijeetagnihotri123 <abhijeetagnihotri123>
2021-03-06 15:57:36 +0530

    more code added

--- amazon_test_seriescodes/trapping_rain_water.cpp
+++ amazon_test_seriescodes/trapping_rain_water.cpp
@@ -1,61 +1,43 @@
-#include<bits/stdc++.h>
-
+#include <iostream>
 using namespace std;
 int max_min(int &a,int &b,bool mode)
 {
-    if(mode)
-    {
-        return a>=b?a:b;
-    }
-    else
-    {
-        return a<=b?a:b;
-    }
+    return mode?(a>=b?a:b):(a<=b?a:b);
 }
-int trappingWater(int a[], int n){
-    
-    int *lm,*rm;
-    int total_water = 0;
+int trappingWater(int[],int);
+int main()
+{
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+        cout<<trappingWater(a,n);
+    }
+    return 0;
+}
+int trappingWater(int a[],int n)
+{
+    int *lm,*rm,total_water,water;
+    total_water = 0;
     lm = new int[n];
     rm = new int[n];
-    lm[0]=a[0];
-    rm[n-1]=a[n-1];
+    lm[0] = a[0];
+    rm[n-1] = a[n-1];
     for(int i=1;i<n;i++)
     {
-        lm[i]=max_min(lm[i-1],a[i],true);
-    }
-    for(int i=n-2;i>=0;i--)
-    {
-        rm[i]=max_min(rm[i+1],a[i],true);
+        lm[i] = max_min(lm[i-1],a[i],true);
+        rm[n-i-1] = max_min(rm[n-i],a[n-i-1],true);
     }
     for(int i=0;i<n;i++)
     {
-        int water = max_min(lm[i],rm[i],false);
+        water = max_min(lm[i],rm[i],false);
         total_water += water-a[i];
     }
-    delete(lm);
-    delete(rm);
     return total_water;
-}
-int main(){
-    
-    int t;
-    cin >> t;
-    
-    while(t--){
-        int n;
-        
-        cin >> n;
-        
-        int a[n];
-        
-        for(int i =0;i<n;i++){
-            cin >> a[i];            
-        }
-        
-        cout << trappingWater(a, n) << endl;
-        
-    }
-    
-    return 0;
 }
