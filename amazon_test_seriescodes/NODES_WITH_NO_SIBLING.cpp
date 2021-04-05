cab70bec5567605926a390e1842a2fa740ba7465
abhijeetagnihotri123 <abhijeetagnihotri123>
2021-04-05 10:52:56 +0530

    more code added

---
+++ amazon_test_seriescodes/NODES_WITH_NO_SIBLING.cpp
@@ -0,0 +1,143 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+vector<int> noSibling(Node *root);
+
+int main()
+{
+    int t;
+    scanf("%d ",&t);
+    while(t--)
+    {
+        string s;
+        getline(cin,s);
+        Node* root = buildTree(s);
+        
+        vector<int> res = noSibling(root);
+        for (int i = 0; i < res.size(); ++i)
+        {
+            cout<<res[i]<<" ";
+        }
+        cout<<endl;
+  }
+  return 0;
+}
+bool check_no_sibling(Node *node)
+{
+    return ((node->left != NULL && node->right == NULL) || (node->left == NULL && node->right != NULL));
+}
+vector<int> noSibling(Node* node)
+{
+    vector<int>res;
+    if(node == NULL)
+    {
+        res = vector<int>(1,-1);
+    }
+    else
+    {
+        queue<Node*>Q;
+        Node *current;
+        Q.push(node);
+        while(!Q.empty())
+        {
+            current = Q.front();
+            Q.pop();
+            if(check_no_sibling(current))
+            {
+                int k = current->left != NULL?current->left->data:current->right->data;
+                res.push_back(k);
+            }
+            if(current->left != NULL)
+            {
+                Q.push(current->left);
+            }
+            if(current->right != NULL)
+            {
+                Q.push(current->right);
+            }
+        }
+        if(res.size() == 0)
+        {
+            res.push_back(-1);
+        }
+        sort(res.begin(),res.end());
+    }
+    return res;
+}
