02f07a76c5a00f4b9fc117e936fa49cc95ba39f8
abhijeetagnihotri123 <31515456+abhijeetagnihotri123@users.noreply.github.com>
2022-06-10 18:45:13 +0530

    Add files via upload

--- amazon_test_seriescodes/mother_vertex.cpp
+++ amazon_test_seriescodes/mother_vertex.cpp
@@ -1,55 +1,98 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int findMother(int,vector<int>[]);
-int main()
-{   
-    int t,a,b,m,n;
-    cin>>t;
-    while(t--)
-    {
-        cin>>a>>b;
-        vector<int>adj[a];
-        for(int i=0;i<b;i++)
-        {
-            cin>>m>>n;
-            adj[m].push_back(n);
-        }
-        cout<<findMother(a,adj)<<endl;
-    }
-    return 0;
-}
-void DFS_UTIL(vector<int>adj[],int v,vector<bool>&visited)
+class Solution 
 {
-    visited[v] = true;
-    for(int i=0;i<adj[v].size();i++)
+    public:
+    /*
+    1st Method did not understand well
+    void DFS_UTIL(vector<int>adj[],int v,vector<bool>&visited)
     {
-        if(!visited[adj[v][i]])
+        visited[v] = true;
+        for(int i=0;i<adj[v].size();i++)
         {
-            DFS_UTIL(adj,adj[v][i],visited);
+            if(!visited[adj[v][i]])
+            {
+                DFS_UTIL(adj,adj[v][i],visited);
+            }
         }
     }
-}
-int findMother(int V,vector<int>adj[])
-{
-    vector<bool>visited(V,false);
-    int last_vertex = 0;
-    for(int i=0;i<V;i++)
+    int findMother(int V,vector<int>adj[])
     {
-        if(!visited[i])
+        vector<bool>visited(V,false);
+        int last_vertex = 0;
+        for(int i=0;i<V;i++)
         {
-            DFS_UTIL(adj,i,visited);
-            last_vertex = i;
+            if(!visited[i])
+            {
+                DFS_UTIL(adj,i,visited);
+                last_vertex = i;
+            }
         }
+        fill(visited.begin(),visited.end(),false);
+        DFS_UTIL(adj,last_vertex,visited);
+        for(int i=0;i<V;i++)
+        {
+            if(!visited[i])
+            {
+                return -1;
+            }
+        }
+        return last_vertex;
     }
-    fill(visited.begin(),visited.end(),false);
-    DFS_UTIL(adj,last_vertex,visited);
-    for(int i=0;i<V;i++)
+    */
+    void DFS(int v,vector<int>adj[],stack<int>&s,vector<bool>&visited)
     {
-        if(!visited[i])
+        visited[v] = true;
+        for(int &x : adj[v])
         {
-            return -1;
+            if(!visited[x])
+            {
+                DFS(x,adj,s,visited);
+            }
         }
+        s.push(v);
     }
-    return last_vertex;
+	int findMotherVertex(int V, vector<int>adj[])
+	{
+        stack<int>s;
+        vector<bool>v(V,false);
+        for(int i=0;i<V;i++)
+        {
+            if(!visited[i])
+            {
+                DFS(i,adj,s,visited);
+            }
+        }
+        fill(visited.begin(),visited.end(),false);
+        int t = s.top();
+        DFS(t,adj,s,visited);
+        for(bool &x : visited)
+        {
+            if(!x)
+            {
+                return -1;
+            }
+        }
+        return t;
+    }
+};
+
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int V, E;
+		cin >> V >> E;
+		vector<int>adj[V];
+		for(int i = 0; i < E; i++){
+			int u, v;
+			cin >> u >> v;
+			adj[u].push_back(v);
+		}
+		Solution obj;
+		int ans = obj.findMotherVertex(V, adj);
+		cout << ans <<"\n";
+	}
+	return 0;
 }
