08411c13b6d213d21351e28e9eb0c95b76fd1605
abhijeetagnihotri123 <31515456+abhijeetagnihotri123@users.noreply.github.com>
2022-12-05 20:19:44 +0530

    Update PALINDROMIC_PARTITION.cpp

--- amazon_test_seriescodes/PALINDROMIC_PARTITION.cpp
+++ amazon_test_seriescodes/PALINDROMIC_PARTITION.cpp
@@ -6,47 +6,47 @@ public:
     int palindromicPartition(string s)
     {
         int n = s.length();
-		bool P[n][n];
-		int C[n];
-		int i,j,L;
-		for(i=0;i<n;i++)
-		{
-			P[i][i] = true;
-		}
+	bool P[n][n];
+	int C[n];
+	int i,j,L;
+	for(i=0;i<n;i++)
+	{
+		P[i][i] = true;
+	}
         for(L = 2; L <= n; L++)
+	{
+		for(i = 0; i < n - L + 1 ; i++)
 		{
-			for(i = 0; i < n - L + 1 ; i++)
+			j = i + L - 1;
+			if(L == 2)
 			{
-				j = i + L - 1;
-				if(L == 2)
-				{
-					P[i][j] = (s[i] == s[j]);
-				}
-				else
-				{
-					P[i][j] = (s[i] == s[j]) && P[i+1][j-1];
-				}
-			}
-		}
-		for(int i=0;i<n;i++)
-		{
-			if(P[0][i])
-			{
-				C[i] = 0;
+				P[i][j] = (s[i] == s[j]);
 			}
 			else
 			{
-				C[i] = INT_MAX;
-				for(int j=0;j<i;j++)
+				P[i][j] = (s[i] == s[j]) && P[i+1][j-1];
+			}
+		}
+	}
+	for(int i=0;i<n;i++)
+	{
+		if(P[0][i])
+		{
+			C[i] = 0;
+		}
+		else
+		{
+			C[i] = INT_MAX;
+			for(int j=0;j<i;j++)
+			{
+				if(P[j+1][i] && (1 + C[j] < C[i]))
 				{
-					if(P[j+1][i] && (1 + C[j] < C[i]))
-					{
-						C[i] = 1 + C[j];
-					}
+					C[i] = 1 + C[j];
 				}
 			}
 		}
-		return C[n-1];
+	}
+	return C[n-1];
     }
 };
 
